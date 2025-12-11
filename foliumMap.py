c0185d282deafab49f1a84b7c2f8086617b22a50
Wasana Perera <wasanapereranew@gmail.com>
2025-12-12 03:01:35 +0530

    fixed error

--- foliumMap.py
+++ foliumMap.py
@@ -1,9 +1,9 @@
 import folium
 
-# Create a map centered at Sri Lanka with a zoom level that shows the full country
-m = folium.Map(location=[7.8731, 80.7718], zoom_start=7)  # Centered at Sri Lanka
 
-# Save the map as an HTML file
+m = folium.Map(location=[7.8731, 80.7718], zoom_start=7)  
+
+
 map_path = 'sri_lanka_map.html'
 m.save(map_path)
 
